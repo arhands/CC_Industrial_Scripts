32d580e6eb935bfb2b8f970654c7973df8522789
arhands <andrewrhands@gmail.com>
2019-05-25 02:50:24 -0500

    did a rough sketch of build quarry cluster

--- QuarryPlacer.lua
+++ QuarryPlacer.lua
@@ -31,6 +31,7 @@ function travelVirtical(distance) do
 end
 --
 --NODES
+--all node builder functions end at the opposite side of the node they build
 --
 function buildCornerNode()
     travelVirtical(-height);
@@ -59,7 +60,7 @@ function buildEdgeNode(horizontal)
     end
 end
 --no quarries or power
-function buildInnerNodeNoQuarry()
+function buildInnerNode()
     for(i = 1, 4, 1) do
         buildCornerNode();
         travel(quarrySpacing);
@@ -69,18 +70,82 @@ function buildInnerNodeNoQuarry()
         turtle.turnLeft();
     end
 end
-
-function buildInnerNodeQuarry()
-    buildInnerNode();
+--upgrades an inner node to an inner node w/ quarry
+function upgradeNodeQuarry()
     turtle.down();
     for(i = 1, 4, 1) do
         turtle.forward();
         turtle.select(cobbleSlot);
         turtle.placeUp();
+
         turtle.back();
         turtle.select(markerSlot);
         turtle.placeUp();
+
         travelVirtical(2 - height);
+        turtle.placeDown();--to activate the quarry shape
+
+        turtle.forward();
+        turtle.select(quarrySlot);
+        turtle.placeDown();
+        travelVirtical(height - 2);
+        travel(quarrySpacing - 1);
+        turtle.turnLeft();
+        travel(quarrySpacing);
+        turtle.turnLeft();
+        turtle.turnLeft();
+    end
+    turtle.up();
+    turtle.turnLeft();
+    turtle.turnLeft();
+end
+
+function buildQuarryCluster(size)
+    --outside
+    for(i = 1, 4, 1) do
+        buildCornerNode();
+        for(j = 2, size, 1) do
+            travel(quarrySize);
+            buildEdgeNode(i % 2 == 0);
+        end
+        if(i < 4) do
+            travel(quarrySize);
+        end
+        turtle.turnRight();
+    end
+    --inside
+    for(i = 2, size, 1) do
+        for(j = 2, size, 1) do
+            travel(quarrySize);
+            buildInnerNode();
+        end
+        if(i % 2 == 0) do
+            turtle.turnRight();
+            travel(quarrySize);
+            turtle.turnRight();
+        else
+            turtle.turnLeft();
+            travel(quarrySize);
+            turtle.turnLeft();
+        end
+    end
+    --place quarries
+    for(i = 2, size, 1) do
+        for(j = 2, size, 1) do
+            travel(quarrySize);
+            if(j % 2 == 0) do
+                upgradeNodeQuarry()
+            end
+        end
+        if(i % 2 == 0) do
+            turtle.turnRight();
+            travel(quarrySize);
+            turtle.turnRight();
+        else
+            turtle.turnLeft();
+            travel(quarrySize);
+            turtle.turnLeft();
+        end
     end
 end
 
